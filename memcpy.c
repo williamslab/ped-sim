d697f07e033af372bcf39adf88c80503882d7c79
Nils Homer <nh13@users.noreply.github.com>
2019-01-12 10:57:36 -0700

    Update to build on macOS

--- memcpy.c
+++ memcpy.c
@@ -2,7 +2,9 @@
 
 // some systems do not have newest memcpy@@GLIBC_2.14 - stay with old good one
 // see https://stackoverflow.com/questions/8823267/linking-against-older-symbol-version-in-a-so-file
+ifdef __linux__
 asm (".symver memcpy, memcpy@GLIBC_2.2.5");
+#endif
 void *__wrap_memcpy(void *dest, const void *src, size_t n)
 {
     return memcpy(dest, src, n);
