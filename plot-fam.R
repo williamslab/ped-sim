7460e4eb28b398a18a6a2f80f9954b8d8d2abced
Amy Williams <awilliams@cornell.edu>
2025-12-06 14:17:37 -0700

    fam plotting: remove the family ids to shorten labels (-keep_fam_id disables)

--- plot-fam.R
+++ plot-fam.R
@@ -1,16 +1,27 @@
 #!/usr/bin/env Rscript
 
 args <- commandArgs(trailingOnly = TRUE)
+rm_fam_id <- TRUE
 
-if (length(args) != 1) {
-  cat("Usage: ./plot-fam.R [base name]\n")
+if (length(args) < 1 || length(args) > 2) {
+  cat("Usage: ./plot-fam.R [-keep_fam_id] <base name>\n")
   quit()
 } else {
+  if (length(args) == 2) {
+    if (args[1] == "-keep_fam_id") {
+      rm_fam_id <- FALSE
+      famfile <- args[2]
+    } else {
+      cat("Usage: ./plot-fam.R [-keep_fam_id] <base name>\n")
+      quit()
+    }
+  } else { # just one command line option -- the filename
+    famfile <- args[1]
+  }
   cat("Generating pdfs with pedigree structures from ", args[1], ".fam...\n\n",
       sep="")
 }
 
-famfile <- args[1]
 
 if (substr(famfile, nchar(famfile)+1-4, nchar(famfile)) != ".fam") {
   famfile <- paste(famfile, ".fam", sep="")
@@ -23,25 +34,33 @@ if (!file.exists(famfile)) {
   quit()
 }
 
-dat <- read.table(famfile)
+fam_df <- read.table(famfile)
+
+if (rm_fam_id) {
+  # remove the family id from each individuals' id -- it's not very helpful for
+  # the plot
+  fam_df[,2] <- substr(fam_df[,2], nchar(fam_df[,1])+2, nchar(fam_df[,2]))
+  fam_df[,3] <- substr(fam_df[,3], nchar(fam_df[,1])+2, nchar(fam_df[,3]))
+  fam_df[,4] <- substr(fam_df[,4], nchar(fam_df[,1])+2, nchar(fam_df[,4]))
+}
 
 #plot pedigree
 library(kinship2)
 
 # Replace 0 parent ids with NA:
-no_dad <- dat[,3]==0
-dat[no_dad,3] <- NA
-no_mom <- dat[,4]==0
-dat[no_mom,4] <- NA
+no_dad <- fam_df[,3]==0
+fam_df[no_dad,3] <- NA
+no_mom <- fam_df[,4]==0
+fam_df[no_mom,4] <- NA
 
-fam_id <- dat[,1]
-status <- rep(1,length(dat[,6]))
-status[dat[,6] != -9] <- 2
+fam_id <- fam_df[,1]
+status <- rep(1,length(fam_df[,6]))
+status[fam_df[,6] != -9] <- 2
 
 for(i in 1:length(fam_id)){
-  sel <- dat[,1]==fam_id[i]
+  sel <- fam_df[,1]==fam_id[i]
 
-  ped <- pedigree(dat[sel,2], dat[sel,3], dat[sel,4], dat[sel,5], status[sel])
+  ped <- pedigree(fam_df[sel,2], fam_df[sel,3], fam_df[sel,4], fam_df[sel,5], status[sel])
 
   pdf(file =paste(args[1], "-", fam_id[i], ".pdf", sep=""), width = 20, height = 8, onefile = TRUE, paper = "special")
   par(xpd=TRUE)
@@ -49,6 +68,6 @@ for(i in 1:length(fam_id)){
   # change cex larger to make the circles/squares and labels larger relative to
   # the lines connecting samples: (may want to increase the margin values
   # in proportion
-  print(plot.pedigree(ped,id=dat[sel,2], mar=c(0,4,1,4), cex=1))
+  print(plot.pedigree(ped,id=fam_df[sel,2], mar=c(0,4,1,4), cex=1))
   dev.off()
 }
